2ce55b50939dd22a122db306ad88bbc6e79acdff
Mahdi267 <mahditzr7@gmail.com>
2024-11-28 22:34:29 -0500

    Ajout de la JavaDoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BitStuffing"},{"p":"<Unnamed>","l":"CRC"},{"p":"<Unnamed>","l":"Frame"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Receiver"},{"p":"<Unnamed>","l":"Sender"},{"p":"<Unnamed>","l":"Timer"}];updateSearchResults();
